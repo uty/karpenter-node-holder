4fb9d1e5074c5c52a862ff3c04b1a2d5ccaac758
Gennadiy Stas <uty.arrghhh@gmail.com>
2023-09-29 14:59:48 +0300

    fix stuff

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
-FROM golang:1.21.1
+FROM golang:1.21.1-alpine
 
 WORKDIR /app
 COPY main.go go.mod go.sum .
 # ENV GO111MODULE=on
 RUN go mod download
-RUN go build -o karpenter-node-holder main.go
+RUN CGO_ENABLED=0 GOOS=linux go build -o karpenter-node-holder main.go
 
 ENV SLEEP_DURATION=15
 ENV HOLD_LABEL="karpenter.sh/do-not-evict"
 
 # Define the command to run the application
-CMD ["./karpernter-node-holder]
+CMD ["./karpenter-node-holder"]
 
--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
   "fmt"
   "os"
   "time"
+  "strconv"
+  "context"
+  metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
   "k8s.io/client-go/kubernetes"
   "k8s.io/client-go/rest"
@@ -38,11 +41,13 @@ func getSleepDuration() int {
 
 func removeLabelFromPod() error {
   config, err := rest.InClusterConfig()
+    fmt.Printf("Got incluster config")
   if err != nil {
     return err
   }
 
   clientset, err := kubernetes.NewForConfig(config)
+    fmt.Printf("Got clientset")
   if err != nil {
     return err
   }
@@ -50,15 +55,18 @@ func removeLabelFromPod() error {
   podName := os.Getenv("POD_NAME")
   namespace := os.Getenv("NAMESPACE")
   holdLabel := os.Getenv("HOLD_LABEL")
+  fmt.Printf("Removing label %s from pod %s in namespace %s\n", holdLabel, podName, namespace)
 
   retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
     pod, err := clientset.CoreV1().Pods(namespace).Get(context.TODO(), podName, metav1.GetOptions{})
     if err != nil {
       return err
     }
+    fmt.Printf("Got pod %s\n", pod.Name)
 
     // Remove the label from pod labels
     delete(pod.Labels, holdLabel)
+    fmt.Printf("Removed label %s from pod %s\n", holdLabel, pod.Name)
 
     _, err = clientset.CoreV1().Pods(namespace).Update(context.TODO(), pod, metav1.UpdateOptions{})
     return err
